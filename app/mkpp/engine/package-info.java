bb47bacd732f79cb230755f07b4856c5e2069998
Mikhail P. Ortiz-Lunyov <108703641+mportizlunyov@users.noreply.github.com>
2024-11-16 15:28:15 -0600

    Added first package-info.java

---
+++ app/mkpp/engine/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Package containing files that actually analyzes text and returns information on it.
+ */
+package app.mkpp.engine;
---
+++ app/mkpp/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * General name of the development team for 2024 CheeseHacks Hackathon.
+ * <br>
+ * M(ikhail), K(eshav), P(arth), (P)rakhar
+ */
+package app.mkpp;
